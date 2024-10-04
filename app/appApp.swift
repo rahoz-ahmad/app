232ddf7d13a3ed18211be26f85902d735c09ca27
rahoz-ahmad <rahozahmad16@gmail.com>
2024-10-04 23:06:23 +0300

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by RAHOZ AHMAD on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ appTests/appTests.swift
@@ -0,0 +1,16 @@
+//
+//  appTests.swift
+//  appTests
+//
+//  Created by RAHOZ AHMAD on 10/4/24.
+//
+
+import Testing
+
+struct appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
